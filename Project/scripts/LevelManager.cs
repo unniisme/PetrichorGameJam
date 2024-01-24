7049f0d26e0c0adaf79b3efaf57f7b024800ee53
unniisme <122001019@smail.iitpkd.ac.in>
2024-01-24 22:58:56 +0530

    Implemented morph charges

---
+++ Project/scripts/LevelManager.cs
@@ -0,0 +1,48 @@
+using Gamelogic.Objects;
+using Godot;
+
+namespace Gamelogic
+{
+    public partial class LevelManager : Node2D
+    {
+        private Player player;
+        private CanvasLayer hud;
+        private int morphCharges = 6;
+
+        [Signal]
+        public delegate void MorphChargesChangedEventHandler(int val);
+
+        public int MorphCharges
+        {
+            get => morphCharges;
+            set
+            {
+                if (morphCharges <= 0) return;
+                morphCharges = value;
+                EmitSignal(SignalName.MorphChargesChanged, value);
+            }
+        }
+
+        public Player Player => player;
+        public CanvasLayer HUD => hud;
+
+        public override void _Ready()
+        {
+            GameManager.RegisterLevel(this);
+
+            player = GetNode<Player>("Player");
+            hud = GetNode<CanvasLayer>("HUD");
+
+            hud.Call("connect_signals");
+        }
+
+        public override void _Process(double delta)
+        {
+            if (MorphCharges > 0 && Input.IsActionJustPressed("action"))
+			{
+                MorphCharges -= 1;
+				GameManager.ToggleMorph();
+			}
+        }
+    }
+}
