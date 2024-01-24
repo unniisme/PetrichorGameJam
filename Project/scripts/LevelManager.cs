365ba7780658e3ffff6403c3633d20dbdbc0a07a
krishannair <krishanvnair2001@example.com>
2024-01-25 02:45:40 +0530

    Main Menu Added

--- Project/scripts/LevelManager.cs
+++ Project/scripts/LevelManager.cs
@@ -3,49 +3,49 @@ using Godot;
 
 namespace Gamelogic
 {
-    public partial class LevelManager : Node2D
-    {
-        private Player player;
-        private CanvasLayer hud;
-        private int morphCharges = 6;
+	public partial class LevelManager : Node2D
+	{
+		private Player player;
+		private CanvasLayer hud;
+		private int morphCharges = 6;
 
-        [Signal]
-        public delegate void MorphChargesChangedEventHandler(int val);
+		[Signal]
+		public delegate void MorphChargesChangedEventHandler(int val);
 
-        [Signal]
-        public delegate void MenuEventHandler();
+		[Signal]
+		public delegate void MenuEventHandler();
 
-        public int MorphCharges
-        {
-            get => morphCharges;
-            set
-            {
-                if (morphCharges <= 0) return;
-                morphCharges = value;
-                EmitSignal(SignalName.MorphChargesChanged, value);
-            }
-        }
-
-        public Player Player => player;
-        public CanvasLayer HUD => hud;
-
-        public override void _Ready()
-        {
-            GameManager.RegisterLevel(this);
-
-            player = GetNode<Player>("Player");
-            hud = GetNode<CanvasLayer>("HUD");
-
-            hud.Call("connect_signals");
-        }
-
-        public override void _Process(double delta)
-        {
-            if (MorphCharges > 0 && Input.IsActionJustPressed("action"))
+		public int MorphCharges
+		{
+			get => morphCharges;
+			set
 			{
-                MorphCharges -= 1;
+				if (morphCharges <= 0) return;
+				morphCharges = value;
+				EmitSignal(SignalName.MorphChargesChanged, value);
+			}
+		}
+
+		public Player Player => player;
+		public CanvasLayer HUD => hud;
+
+		public override void _Ready()
+		{
+			GameManager.RegisterLevel(this);
+
+			player = GetNode<Player>("Player");
+			hud = GetNode<CanvasLayer>("HUD");
+
+			hud.Call("connect_signals");
+		}
+
+		public override void _Process(double delta)
+		{
+			if (MorphCharges > 0 && Input.IsActionJustPressed("action"))
+			{
+				MorphCharges -= 1;
 				GameManager.ToggleMorph();
 			}
-        }
-    }
+		}
+	}
 }
