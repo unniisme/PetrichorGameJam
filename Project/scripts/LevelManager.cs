ddc37004d4ffccb4bb37845cf9c1af3933b27422
unniisme <122001019@smail.iitpkd.ac.in>
2024-01-31 19:09:38 +0530

    Autoload audiomanager

--- Project/scripts/LevelManager.cs
+++ Project/scripts/LevelManager.cs
@@ -1,3 +1,4 @@
+using Gamelogic.Audio;
 using Gamelogic.Objects;
 using Godot;
 
@@ -73,13 +74,6 @@ namespace Gamelogic
 			cam ??= GetNode<Camera2D>("Camera");
 
 			MorphCharges += GameManager.leftoverCrystals;
-			var backMusic = GetNode<AudioStreamPlayer>("HappyBackground");
-			if(backMusic != null)
-			{
-				backMusic.Stream = GD.Load<AudioStream>("res://assets/Sounds/Dr. Mario 64 - Story Mode 3.mp3");
-				backMusic.Play();
-				
-			}
 			hud.Call("initialize");
 		}
 
@@ -91,21 +85,16 @@ namespace Gamelogic
 			if (MorphCharges > 0 && Input.IsActionJustPressed("action"))
 			{
 				MorphCharges -= 1;
-				var usedCrystal = GetNode<AudioStreamPlayer>("UsedCrystal");
-				var reusedCrystal = GetNode<AudioStreamPlayer>("ReusedCrystal");
-				if(usedCrystal != null && reusedCrystal != null)
+
+				if (MorphCharges%2 == 0)
 				{
-					usedCrystal.Stream = GD.Load<AudioStream>("res://assets/Sounds/impact-6291.mp3");
-					reusedCrystal.Stream = GD.Load<AudioStream>("res://assets/Sounds/fast-whoosh-118248.mp3");
-					if(MorphCharges % 2 == 0)
-					{
-						reusedCrystal.Play();
-					}
-					else
-					{
-						usedCrystal.Play();
-					}
+					AudioManager.PlayStream("reusedCrystal");
 				}
+				else
+				{
+					AudioManager.PlayStream("usedCrystal");
+				}
+
 				GameManager.ToggleMorph();
 			}
 		}
---
+++ Project/scripts/autoloads/AudioManager.cs
@@ -0,0 +1,42 @@
+using Godot;
+using System;
+using System.Collections.Generic;
+
+namespace Gamelogic.Audio
+{
+    public partial class AudioManager : Node2D
+    {
+        public static readonly Dictionary<StringName, AudioStreamPlayer> audioStreams = new();
+
+        public override void _Ready()
+        {
+            foreach (Node child in GetChildren())
+            {
+                if (child is AudioStreamPlayer stream)
+                {
+                    audioStreams[stream.Name] = stream;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Get a certain audio stream player
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        public static AudioStreamPlayer GetStreamPlayer(StringName name)
+        {
+            return audioStreams[name];
+        }
+
+
+        /// <summary>
+        /// Play a certain audio stream player
+        /// </summary>
+        /// <param name="name"></param>
+        public static void PlayStream(StringName name)
+        {
+            audioStreams[name].Play();
+        }
+    }
+}
